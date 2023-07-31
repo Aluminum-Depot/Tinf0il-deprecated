9d17c6ab858a10e59c2595089073684012fbef53
Citrik <64604942+Citrik2062@users.noreply.github.com>
2023-07-31 12:39:50 -0400

    Update games.js

--- Ultraviolet-Static/public/data/games.js
+++ Ultraviolet-Static/public/data/games.js
@@ -334,7 +334,7 @@ var games = [
     {
       'id': 'roblox',
       'title': "Roblox",
-      'url': 'https://nowgg.me',
+      'url': 'https://now.gg',
       'image': 'https://play-lh.googleusercontent.com/WNWZaxi9RdJKe2GQM3vqXIAkk69mnIl4Cc8EyZcir2SKlVOxeUv9tZGfNTmNaLC717Ht'
     },
   ]

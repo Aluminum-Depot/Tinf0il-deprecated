779ba1080e526ddd6c9d8595af5af5bed9e0e8b0
Citrik <64604942+Citrik2062@users.noreply.github.com>
2023-08-11 13:56:36 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ server.on("upgrade", (req, socket, head) => {
 
 let port = parseInt(process.env.PORT || "");
 
-if (isNaN(port)) port = 8080;
+if (isNaN(port)) port = 80;
 
 server.on("listening", () => {
   const address = server.address();
